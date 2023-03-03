1f902ab6589d44792bbbdd973af7eeb423dc35b5
Candra <99946773+candra432@users.noreply.github.com>
2023-03-03 21:24:27 +0900

    Update gcnban_anomaly_detection.py

--- gcnban_anomaly_detection.py
+++ gcnban_anomaly_detection.py
@@ -112,7 +112,7 @@ for label in list(df_cat['proto'].value_counts().index)[::-1][1:]:
 
 # Split
 X_train, X_test, y_train, y_test = train_test_split(X, y,
-                                                    test_size = 0.175, 
+                                                    test_size = 0.2, 
                                                     random_state = 0,
                                                     stratify=y)
 
